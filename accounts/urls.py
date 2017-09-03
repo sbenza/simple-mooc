89a7b295d2dcc6aa4af50eb9de307a11d8dcb77f
Silvia Benza <silviabenza@gmail.com>
2017-09-03 16:14:38 -0300

    login/logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     url(r'^entrar/$', auth_views.login, {'template_name':'accounts/login.html'}, name='login'),
     url(r'^cadastre-se/$', views.register, name='register'),
+    url(r'^sair/$', auth_views.logout, {'next_page':'core:home'}, name='logout'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
+from django.contrib.auth import authenticate,login
 
 from .forms import RegisterForm
 # Create your views here.
@@ -10,11 +11,16 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect(settings.LOGIN_URL)
+            user = form.save()
+            user = authenticate(
+                username = user.username , password = form.cleaned_data['password1']
+            )
+            login(request,user)
+            return redirect('core:home')
     else:
         form = RegisterForm()
     context={
             'form': form
         }
     return render(request,template_name,context)
+
